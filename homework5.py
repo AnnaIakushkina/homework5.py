d86a9d60fcb3f8153f5b5f9130d6b3403dc61485
AnnaIakushkina <barbakadze.anna@bk.ru>
2024-05-21 18:09:29 +0300

    homework5

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ['aplle', 'banana', 'avocado', 'cherry', 'melon']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[3:5])
+my_list[3] = 'kiwi'
+print(my_list)
+
+
+my_dict = {'melon': 'дыня', 'kiwi': 'киви', 'cherry': 'вишня', 'avocado': 'авокадо'}
+print(my_dict)
+print(my_dict.values())
+my_dict['watermelon'] = 'арбуз'
+print(my_dict)
